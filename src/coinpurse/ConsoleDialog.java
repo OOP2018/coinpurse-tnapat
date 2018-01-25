e37eba3694e58844523ff53a41888857883334e7
Napat Thongpao <npt.thongpao@gmail.com>
2018-01-25 11:08:10 +0700

    push new user

--- src/coinpurse/ConsoleDialog.java
+++ src/coinpurse/ConsoleDialog.java
@@ -1,7 +1,7 @@
 package coinpurse;
  
 import java.util.Scanner;
-// test push
+// test push new user
 /** 
  * User Interface for the Coin Purse. 
  * This class provides simple interactive dialog for inserting
